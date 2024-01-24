4f58506af8806ae48a143afc1c89d600605f3804
Alex <lxrmrz732@gmail.com>
2024-01-24 17:32:17 -0500

    finished? bfs thanks to stryder algorithm

--- apps/backend/src/algorithms/Graph.ts
+++ apps/backend/src/algorithms/Graph.ts
@@ -1,105 +1,97 @@
-import {node} from "./node.ts";
-import {edge} from "./edge.ts";
+import { node } from "./node.ts";
+import { edge } from "./edge.ts";
 
 /*
-*
-*
-*
-*
-* */
+ *
+ *
+ *
+ *
+ * */
 export class Graph {
-    private readonly nodes: Array<node>;
-    private readonly edges: Array<edge>;
-    private readonly adjacent: Map<node, Array<node>>;
-    private readonly idLookup: Map<string, node>;
+  private readonly nodes: Array<node>;
+  private readonly edges: Array<edge>;
+  private readonly adjacent: Map<node, Array<node>>;
+  private readonly idLookup: Map<string, node>;
 
-    constructor(unLinkedNodes: Array<node>, edges: Array<edge>) {
-        //create map
-        const nodeMap = new Map<string, node>();
+  constructor(unLinkedNodes: Array<node>, edges: Array<edge>) {
+    //create map
+    const nodeMap = new Map<string, node>();
 
-        const adj = new Map<node, Array<node>>();
+    const adj = new Map<node, Array<node>>();
 
-        unLinkedNodes.forEach(function (node) {
-            nodeMap.set(node.iD, node);
-            adj.set(node,new Array<node>());
-        });
+    unLinkedNodes.forEach(function (node) {
+      nodeMap.set(node.iD, node);
+      adj.set(node, new Array<node>());
+    });
 
-        edges.forEach(function (edge) {
-            const startNode = nodeMap.get(edge.startNode.iD) ?? null;
-            const endNode = nodeMap.get(edge.endNode.iD) ?? null;
+    edges.forEach(function (edge) {
+      const startNode = nodeMap.get(edge.startNode.iD) ?? null;
+      const endNode = nodeMap.get(edge.endNode.iD) ?? null;
 
-            if (startNode == null) {
-                console.error("startNode for edge not found");
-                return;
-            }
-            if (endNode == null) {
-                console.error("endNode for edge not found");
-                return;
-            }
+      if (startNode == null) {
+        console.error("startNode for edge not found");
+        return;
+      }
+      if (endNode == null) {
+        console.error("endNode for edge not found");
+        return;
+      }
 
-            // replace node in edge with correct Node
-            edge.startNode = startNode;
-            edge.endNode = endNode;
+      // replace node in edge with correct Node
+      edge.startNode = startNode;
+      edge.endNode = endNode;
 
-            startNode.edges.push(edge);
+      startNode.edges.push(edge);
 
-            // create reverse edge for other node
-            const backEdge: edge = {
-                iD: edge.endNode.iD + "_" + edge.startNode.iD,
-                startNode: edge.endNode,
-                endNode: edge.startNode,
-            };
+      // create reverse edge for other node
+      const backEdge: edge = {
+        iD: edge.endNode.iD + "_" + edge.startNode.iD,
+        startNode: edge.endNode,
+        endNode: edge.startNode,
+      };
 
-            endNode.edges.push(backEdge);
+      endNode.edges.push(backEdge);
 
+      //make adjlist
+      const startAdj = adj.get(startNode) ?? null;
+      const endAdj = adj.get(endNode) ?? null;
 
-            //make adjlist
-            const startAdj =  adj.get(startNode) ?? null;
-            const endAdj =  adj.get(endNode) ?? null;
+      if (startAdj == null) {
+        console.error("startAdj for node not found");
+        return;
+      }
+      if (endAdj == null) {
+        console.error("endAdj for node not found");
+        return;
+      }
+      startAdj.push(endNode);
 
-            if(startAdj == null){
-                console.error("startAdj for node not found");
-                return;
-            }
-            if(endAdj == null){
-                console.error("endAdj for node not found");
-                return;
-            }
-            startAdj.push(endNode);
+      endAdj.push(startNode);
+    });
 
-            endAdj.push(startNode);
+    this.nodes = unLinkedNodes;
+    this.edges = edges;
+    this.adjacent = adj;
+    this.idLookup = nodeMap;
+  }
 
+  public getNodes(): Array<node> {
+    return this.nodes;
+  }
 
+  public getEdges(): Array<edge> {
+    return this.edges;
+  }
 
+  public getAdjList() {
+    return this.adjacent;
+  }
 
-        });
+  public idToNode(str: string) {
+    return this.idLookup.get(str) ?? null;
+  }
 
-        this.nodes = unLinkedNodes;
-        this.edges = edges;
-        this.adjacent = adj;
-        this.idLookup=nodeMap;
-    }
-
-    public getNodes(): Array<node> {
-        return this.nodes;
-    }
-
-    public getEdges(): Array<edge> {
-        return this.edges;
-    }
-
-    public getAdjList(){
-        return this.adjacent;
-    }
-
-    public idToNode(str:string){
-        return this.idLookup.get(str) ?? null;
-    }
-
-
-    public adjacentTo(someNode: node) : Array<node>|null {
-
-        return this.adjacent.get(someNode) ?? null;
-    }
-
+  public adjacentTo(someNode: node): Array<node> | null {
+    return this.adjacent.get(someNode) ?? null;
+  }
 }
--- apps/backend/src/algorithms/bfs/bfs.ts
+++ apps/backend/src/algorithms/bfs/bfs.ts
@@ -1,4 +1,4 @@
-/* Shitty BFS by Alex */
+/* BASED BFS by Alex and Stryder */
 import { node } from "../node.ts";
 import { Graph } from "../Graph.ts";
 import { CrapQueue } from "../queue/CrapQueue.ts";
@@ -10,36 +10,83 @@ import { IQueue } from "../queue/IQueue.ts";
  * Breadth-first search algorithm to find a path from source to target.
  *
  * Returns a list of the path from source to target if it exists or null
- * if no such path exists.
+ * if no such path exists in graph.
  */
-export function bfs(source: node, target: node, graph: Graph): Array<node> | null {
+export function bfs(
+  source: node | null,
+  target: node | null,
+  graph: Graph,
+): Array<node> | null {
+  /* garbage */
+  if (source == null) {
+    console.error("source in bfs was null");
+    return null;
+  }
+  if (target == null) {
+    console.error("target in bfs was null");
+    return null;
+  }
+
   /* symbols */
+  let path_found: boolean = false; // flag to construct a path
   const frontier: IQueue<node> = new CrapQueue<node>(); // frontier for next nodes to visit
-  const visited: Map<node, boolean> = new Map<node, boolean>(); // visited nodes to prevent cycles
-  const adjacent: Map<node, Array<node>> = new Map<node, Array<node>>(); // needed?
+  const predecessor: Map<node, node | null> = new Map<node, node | null>(); // visited nodes to prevent cycles
   const path: Array<node> = new Array<node>();
 
-  /* initially populate the frontier with source's neighbors */
-  path.push(source);
-  visited.set(source, true);
-  for (let i: number = 0; i < source.edges.length; i++) {
-    frontier.push(source.edges[i].endNode);
-  }
+  /* add the source to the frontier initially */
+  frontier.push(source);
+  predecessor.set(source, null); // mark the source as visited
 
   /* while the frontier is not empty */
   while (frontier.hasItems()) {
     /* pop the current node */
-    const currentNode: node | undefined = frontier.pop();
-    if (currentNode == undefined) {
+    const current_node: node | undefined = frontier.pop();
+    if (current_node == undefined) {
+      console.error("dude how did you mess up the queue in BFS in IQueue");
       break; // idk
     }
 
-    /* if we've already seen this node */
-    if (visited.get(currentNode)) {
-      continue;
+    /* check if the current node happens to be the target */
+    if (current_node.iD == target.iD) {
+      /* bottle of Kool-Aid; LFG we're done */
+      path_found = true;
+      break;
     }
-    visited.set(currentNode, true);
+
+    /* find the adjacent nodes to currentNode */
+    const adjacent_nodes: Array<node> | null = graph.adjacentTo(current_node);
+    if (adjacent_nodes != null) {
+      /* for each node adjacent to current_node */
+      for (let i: number = 0; i < adjacent_nodes.length ?? 0; i++) {
+        /* if the neighbor has not yet been encountered */
+        if (!predecessor.has(adjacent_nodes[i])) {
+          /* add it to the frontier */
+          frontier.push(adjacent_nodes[i]);
+          /* put it in the predecessors */
+          predecessor.set(adjacent_nodes[i], current_node);
+        }
+      }
+    }
   }
 
+  /* if we found the way to the end */
+  if (path_found) {
+    /* construct the path from the predecessor map from the target-back */
+    let current_predecessor: node | null | undefined = target;
+
+    /* the predecessor to the source node is null */
+    while (current_predecessor != null) {
+      /* push the current predecessor to the found path */
+      path.push(current_predecessor);
+
+      /* update the predecessor */
+      current_predecessor = predecessor.get(current_predecessor);
+    }
+
+    /* whoops */
+    return path.reverse();
+  }
+
+  /* no path was found */
   return null;
 }
