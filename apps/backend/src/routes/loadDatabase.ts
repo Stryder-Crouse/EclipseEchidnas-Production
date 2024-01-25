c4fe79c62b30e106dfd98461bad9dec688b81d12
Szymon <szymonmamro@gmail.com>
2024-01-24 21:04:47 -0500

    Switched the nodeArray.forEach function we had initially to Promise.all which is better I guess. And it seems as though using connect: nodeData.edges allows to correctly set the start and end edges

--- apps/backend/src/routes/loadDatabase.ts
+++ apps/backend/src/routes/loadDatabase.ts
@@ -1,8 +1,7 @@
-/*
 import express, { Router, Request, Response } from "express";
-import { Prisma } from "database"; //may be very wrong
+//import { Prisma } from "database"; //may be very wrong
 import { node } from "../algorithms/node.ts";
-import { coordinate } from "../algorithms/coordinate.ts";
+//import { coordinate } from "../algorithms/coordinate.ts";
 import { edge } from "../algorithms/edge.ts";
 import { readNodeCSV } from "../algorithms/readCSV.ts";
 import { readEdgeCSVNOLINK } from "../algorithms/readCSV.ts";
@@ -12,83 +11,87 @@ import fs from "fs";
 
 const router: Router = express.Router();
 
-async function main() {
-  router.post("/", async function (req: Request, res: Response) {
-    let allNodeString = "";
-    let allEdgeString = "";
+router.post("/", async function (req: Request, res: Response) {
+  let allNodeString = "";
+  let allEdgeString = "";
 
-    //NODES
-    try {
-      const csvLocation = path.resolve(
-        __dirname,
-        "../../resources/L1Nodes.csv",
-      );
-      allNodeString = fs.readFileSync(csvLocation, "utf-8");
-    } catch (error) {
-      console.error("CVS node file not found");
-      res.status(204); // and send 204
-      return;
-    }
-    const nodeArray: node[] = readNodeCSV(allNodeString);
+  //NODES
+  try {
+    const csvLocation = path.resolve(__dirname, "../../resources/L1Nodes.csv");
+    allNodeString = fs.readFileSync(csvLocation, "utf-8");
+  } catch (error) {
+    console.error("CVS node file not found");
+    res.status(204); // and send 204
+    return;
+  }
+  const nodeArray: node[] = readNodeCSV(allNodeString);
 
-    nodeArray.forEach((node) => {
+  await Promise.all(
+    nodeArray.map(async (nodeData) => {
       try {
         // Attempt to create in the database
         await PrismaClient.node.create({
           data: {
-            nodeID: node.iD,
-            floor: node.floor,
-            building: node.building,
-            nodeType: node.nodeType,
-            longName: node.longName,
-            shortName: node.shortName,
-            xcoord: node.coordinate.x,
-            ycoord: node.coordinate.y,
-            startEdges: node.edges,
-            endEdges: node.edges,
+            nodeID: nodeData.iD,
+            floor: nodeData.floor,
+            building: nodeData.building,
+            nodeType: nodeData.nodeType,
+            longName: nodeData.longName,
+            shortName: nodeData.shortName,
+            xcoord: nodeData.coordinate.x,
+            ycoord: nodeData.coordinate.y,
+            startEdges: {
+              connect: nodeData.edges.map((edge) => ({ edgeID: edge.iD })),
+            },
+            endEdges: {
+              connect: nodeData.edges.map((edge) => ({ edgeID: edge.iD })),
+            },
           },
         }); //this line should be changed, also
         console.info("Successfully saved node"); // Log that it was successful
       } catch (error) {
         // Log any failures
-        console.error(`Unable to save node ${node}: ${error}`);
+        console.error(`Unable to save node ${nodeData.iD}: ${error}`);
         res.sendStatus(400); // Send error
         return; // Don't try to send duplicate statuses
       }
-    });
+    }),
+  );
 
-    //EDGES
-    try {
-      const csvLocation = path.resolve(
-        __dirname,
-        "../../resources/L1Edges.csv",
-      );
-      allEdgeString = fs.readFileSync(csvLocation, "utf-8");
-    } catch (error) {
-      console.error("CVS edge file not found");
-      res.status(204); // and send 204
-      return;
-    }
-    const edgeArray: edge[] = readEdgeCSVNOLINK(allEdgeString);
+  //EDGES
+  try {
+    const edgeCsvLocation = path.resolve(
+      __dirname,
+      "../../resources/L1Edges.csv",
+    );
+    allEdgeString = fs.readFileSync(edgeCsvLocation, "utf-8");
+  } catch (error) {
+    console.error("CVS edge file not found");
+    res.status(204); // and send 204
+    return;
+  }
+  const edgeArray: edge[] = readEdgeCSVNOLINK(allEdgeString);
 
-    edgeArray.forEach((edge) => {
+  await Promise.all(
+    edgeArray.map(async (edgeData) => {
       try {
         // Attempt to create in the database
         await PrismaClient.edge.create({
           data: {
-            edgeID: edge.iD,
-            startNode: edge.startNode,
-            endNode: edge.endNode,
+            edgeID: edgeData.iD,
+            startNode: { connect: { nodeID: edgeData.startNode.iD } },
+            endNode: { connect: { nodeID: edgeData.endNode.iD } },
           },
         }); //this line should be changed, also
         console.info("Successfully saved edge"); // Log that it was successful
       } catch (error) {
         // Log any failures
-        console.error(`Unable to save edge ${edge}: ${error}`);
+        console.error(`Unable to save edge ${edgeData.iD}: ${error}`);
         res.sendStatus(400); // Send error
         return; // Don't try to send duplicate statuses
       }
-    });
-  });
-}
-*/
+    }),
+  );
+});
+
+export default router;
