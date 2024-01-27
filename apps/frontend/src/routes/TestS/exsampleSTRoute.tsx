b778c2d6a8d8ef9bc8e45e6079f58977be0083da
Stryder Crouse <strydercrouse@gmail.com>
2024-01-27 17:47:23 -0500

    removed code that defined the defult image of html as the hosbital image and the defult color as a blue (we should not have that there)

--- apps/frontend/src/routes/TestS/exsampleSTRoute.tsx
+++ apps/frontend/src/routes/TestS/exsampleSTRoute.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ExampleComponentStryderNode } from "../../components/TestS/ExampleComponentStryderNode.tsx";
 import { ExampleComponentStryderEdge } from "../../components/TestS/ExampleComponentStryderEdge.tsx";
 
 //stryders test element
 export default function Stryderstestroute() {
+  useEffect(() => {
+    //set background to floor on component load
+    document.body.style.backgroundImage = "none";
+  }, []);
   return (
     <div className="w-100 h-100 d-flex flex-column overflow-auto">
       <h1>SUPMAN.</h1>
