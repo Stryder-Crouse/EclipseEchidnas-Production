5811694ed125d97069253a3268e5cbb7bb54f94a
Stryder Crouse <strydercrouse@gmail.com>
2024-01-29 18:21:02 -0500

    created and SVG exsample map

--- apps/frontend/src/components/TestS/exampleMap.tsx
+++ apps/frontend/src/components/TestS/exampleMap.tsx
@@ -36,45 +36,52 @@ async function makeNodesExsample() {
   //aTag.appendChild(newRect);
   aTag.appendChild(newRect);
   map.appendChild(aTag);
-  textInElement("g", "d");
+  //textInElement("g", "d");
 }
 
+/**
+ * creates the node objects on the map though html DOM
+ *
+ */
 async function makeNodes() {
+  //load nodes from file CHANGE LAYER
   const nodes: Array<node> = readNodeCSV(await getNodeCSVString());
 
-  console.log(nodes);
-
+  //find svg map
   const map = document.getElementById("map");
+
+  //for each node add it to the file
   nodes.forEach(function (newNode: node) {
+    //atag to contain link / make it clickable also contatins the circle within it
     const aTag = document.createElementNS("http://www.w3.org/2000/svg", "a");
+    //circle to show node
     const newNodeCircle = document.createElementNS(
       "http://www.w3.org/2000/svg",
       "circle",
     );
+    //middle coordnate from circle
     newNodeCircle.setAttribute("cx", newNode.coordinate.x.toString());
     newNodeCircle.setAttribute("cy", newNode.coordinate.y.toString());
+    //id is node id
     newNodeCircle.setAttribute("id", newNode.iD);
+    //circle radius
     newNodeCircle.setAttribute("r", "10");
+    //color
     newNodeCircle.setAttribute("fill", "blue");
 
+    //give a link to the a tag
     aTag.setAttribute(
       "href",
       "https://www.youtube.com/watch?v=uRlmjiwgbXY&ab_channel=BrosFOURRSpeed",
     );
+    //add circle to a tag
     aTag.appendChild(newNodeCircle);
     if (map == null) {
       return;
     }
+    //add a tag to map
     map.appendChild(aTag);
   });
-  //aTag.appendChild(newRect);
-  textInElement("g", "d");
-}
-
-function textInElement(text: string, element: string) {
-  const elem = document.createElement(element);
-  elem.textContent = text;
-  return elem;
 }
 
 async function getEdgeCSVString(): Promise<string> {
