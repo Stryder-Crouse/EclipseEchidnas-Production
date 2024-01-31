d558b66be76b0ea23250a600062c01f23a691867
ajaguiar <ajaguiar1007@gmail.com>
2024-01-31 16:00:06 -0500

    put everything together

--- apps/frontend/src/routes/ServicePage.tsx
+++ apps/frontend/src/routes/ServicePage.tsx
@@ -44,45 +44,58 @@ export default function ServicePage() {
   }
 
   return (
-    <div className={"servicePage grid"}>
-      <form>
-        <input
-          type={"radio"}
-          id={"medRequest"}
-          name={"request"}
-          value={"Medicine Request"}
-        />
-        <label form={"medRequest"}>Medicine Request</label>
-        <br />
-        <input
-          type={"text"}
-          id={"medRequestLocal"}
-          name={"medRequestLocal"}
-          placeholder={"Location"}
-          value={medRequestLocale}
-          onChange={handleMedRequestLocaleInput}
-        />
-        <br />
-        <input
-          type={"text"}
-          id={"medRequestType"}
-          name={"medRequestType"}
-          placeholder={"Medicine Type"}
-          value={medRequestType}
-          onChange={handleMedRequestTypeInput}
-        />
-        <br />
-        <input
-          type={"text"}
-          id={"medRequestDose"}
-          name={"medRequestDoseage"}
-          placeholder={"Medicine Doseage"}
-          value={medRequestDoses}
-          onChange={handleMedRequestDoseInput}
-        />
-        <br />
-        <RequestButtons submit={submit} />
-      </form>
+    <div className={"servicePage"}>
+      <div className={"grid"}>
+        <div className={"title"}>
+          <h1>Service Request</h1>
+        </div>
+        <div className={"servDropdown"}>
+          <select name={"services"} id={"service"}>
+            <option>Select Request...</option>
+            <option>Medicine Request</option>
+          </select>
+
+          {/*<input*/}
+          {/*  type={"radio"}*/}
+          {/*  id={"medRequest"}*/}
+          {/*  name={"request"}*/}
+          {/*  value={"Medicine Request"}*/}
+          {/*/>*/}
+        </div>
+        <div className={"locale"}>
+          <input
+            type={"text"}
+            id={"medRequestLocale"}
+            name={"medRequestLocale"}
+            placeholder={"Location"}
+            value={medRequestLocale}
+            onChange={handleMedRequestLocaleInput}
+          />
+        </div>
+        <div className={"medName"}>
+          <input
+            type={"text"}
+            id={"medRequestType"}
+            name={"medRequestType"}
+            placeholder={"Medicine Name"}
+            value={medRequestType}
+            onChange={handleMedRequestTypeInput}
+          />
+        </div>
+        <div className={"medDoses"}>
+          <input
+            type={"text"}
+            id={"medRequestDose"}
+            name={"medRequestDoseage"}
+            placeholder={"Medicine Doseage"}
+            value={medRequestDoses}
+            onChange={handleMedRequestDoseInput}
+          />
+        </div>
+        <div className={"submitButton"}>
+          <RequestButtons submit={submit} />
+        </div>
+      </div>
     </div>
   );
 }
