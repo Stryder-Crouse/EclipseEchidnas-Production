5e11a214e1d8f5e20ac042337a2c7ef8ed13299d
Stryder Crouse <strydercrouse@gmail.com>
2024-01-31 22:06:03 -0500

    moved locatios drop down into its own componate as well as the population code for it. (this advoids dupilate code in guest and admin map pages)

--- apps/frontend/src/components/LocationsDropDown.tsx
+++ apps/frontend/src/components/LocationsDropDown.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./component-css/NavBar.css";
+import axios from "axios";
+import { Edge } from "../../../backend/src/algorithms/Graph/Edge.ts";
+import { Node } from "../../../backend/src/algorithms/Graph/Node.ts";
 
 export default function LocationsDropDown() {
   const [showDropdown, setShowDropdown] = useState(false);
@@ -20,4 +23,121 @@ export default function LocationsDropDown() {
       ></div>
     </div>
   );
+}
+
+//populate the dropdown with locations on page load
+submitNodes().then(() => {
+  submitEdges().then();
+}); //populates the Node table
+populateLocationDropdown().then();
+//submitEdges().then();
+//submitEdges(); //populates the Edge table
+getNodes().then(); //send a get request to server and received info about all nodes (uncomment to use)
+getEdges().then();
+
+/**
+ * This function populates the Dropdown div with the locations represented by nodes.
+ * SHOULD ONLY BE RUN ONCE.
+ */
+async function populateLocationDropdown() {
+  //read node file and create the nodes
+  const nodes = await getNodes().then();
+
+  //MAKE ALPHABETICAL
+
+  //console.log("nodes");
+  //console.log(nodes);
+  //fine dropdown div in the html on the page
+  const myDropdown = document.getElementById("myDropdown");
+  //console.log("myDropdown");
+  //console.log(myDropdown);
+  //for each node
+  nodes.forEach(function (newNode: Node) {
+    //create a element
+    const row = document.createElement("a");
+
+    //use longName of node as the text content for new a tag
+    row.textContent = newNode.longName;
+
+    if (myDropdown == null) {
+      return;
+    }
+
+    //add new a element to dropdown
+    myDropdown.appendChild(row);
+  });
+}
+
+/**
+ *
+ * @returns a node array of nodes in the database
+ *
+ */
+async function getNodes() {
+  try {
+    //MAKE SURE TO PASS THE TYPE axios.get<TYPE> to get the data out correctly
+    //the node[] make the data a node array type (axios does the conversion for us)
+    const response = await axios.get<Node[]>("/api/load-nodes");
+    console.log(response);
+    return response.data;
+  } catch (err) {
+    throw new Error("Error getting Nodes");
+  }
+}
+
+/**
+ *
+ *
+ * @returns an edge array of nodes in the database
+ *
+ */
+async function getEdges() {
+  try {
+    const response = await axios.get<Edge[]>("/api/load-edges");
+    console.log(response);
+    console.log(response.data);
+    //create node obj from responses
+    //response.data;
+  } catch (err) {
+    throw new Error("Error getting Edges");
+  }
+}
+
+/**
+ *
+ * loads nodes into the database
+ *
+ * */
+async function submitNodes() {
+  const data = JSON.stringify({}); //making a JSON format file from input Nodes
+  console.log(data);
+  //sends a post request the /api/load-db     (server accessing this api)
+  try {
+    await axios.post("/api/load-nodes", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err) {
+    throw new Error("Error with loading Nodes");
+  }
+}
+/**
+ *
+ * loads edges into the database
+ *
+ * */
+async function submitEdges() {
+  const data = JSON.stringify({}); //making a JSON format file from input Nodes
+  console.log(data);
+  //sends a post request the /api/load-db     (server accessing this api)
+  try {
+    await axios.post("/api/load-edges", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err) {
+    throw new Error("Error with loading Edges");
+  }
 }
