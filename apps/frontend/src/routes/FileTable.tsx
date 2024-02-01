3f3274808b39ce36603d34f0e62fcc277fe24f77
Stryder Crouse <strydercrouse@gmail.com>
2024-02-01 02:50:19 -0500

    stryder -- added fount end prepcode for some pages

--- apps/frontend/src/routes/FileTable.tsx
+++ apps/frontend/src/routes/FileTable.tsx
@@ -2,6 +2,8 @@ import "../css/file-table.css";
 import React, { useEffect } from "react";
 import ExitButton from "../components/ExitButton.tsx";
 import AdminPageNavBar from "../components/AdminPageNavBar.tsx";
+import NodeTable from "../components/NodeAndEdgeTable/NodeTable.tsx";
+import EdgeTable from "../components/NodeAndEdgeTable/EdgeTable.tsx";
 
 function FileTable() {
   useEffect(() => {
@@ -12,78 +14,27 @@ function FileTable() {
 
   return (
     <div>
-      <AdminPageNavBar />
-      <div className={"table-container"}>
-        <span className={"caption-container"}>
-          <span className={"table-title"}>Current .CSV File</span>
-        </span>
-        <div className={"table-wrapper"}>
-          <table>
-            <thead>
-              <tr>
-                <th>nodeID</th>
-                <th>xcoord</th>
-                <th>ycoord</th>
-                <th>floor</th>
-                <th>building</th>
-                <th>nodeType</th>
-                <th>longName</th>
-                <th>shortName</th>
-              </tr>
-            </thead>
-            <tbody id={"table-rows"}>
-              <tr>
-                <td className={"node-id"}>CCONF001L1</td>
-                <td>2255</td>
-                <td>849</td>
-                <td>L1</td>
-                <td>45 Francis</td>
-                <td>CONF</td>
-                <td>Anesthesia Conf Floor L1</td>
-                <td>Conf C001L1</td>
-              </tr>
-              <tr>
-                <td className={"node-id"}>CCONF002L1</td>
-                <td>2665</td>
-                <td>1043</td>
-                <td>L1</td>
-                <td>45 Francis</td>
-                <td>CONF</td>
-                <td>Medical Records Conference Room Floor L1</td>
-                <td>Conf C002L1</td>
-              </tr>
-              <tr>
-                <td className={"node-id"}>CCONF003L1</td>
-                <td>2445</td>
-                <td>1245</td>
-                <td>L1</td>
-                <td>45 Francis</td>
-                <td>CONF</td>
-                <td>Abrams Conference Room</td>
-                <td>Conf C003L1</td>
-              </tr>
-              <tr>
-                <td className={"node-id"}>CDEPT002L1</td>
-                <td>1980</td>
-                <td>844</td>
-                <td>L1</td>
-                <td>Tower</td>
-                <td>DEPT</td>
-                <td>Day Surgery Family Waiting Floor L1</td>
-                <td>Department C002L1</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <div>
+        <AdminPageNavBar />
+      </div>
 
-        <div className={"import-and-export"}>
-          <button className={"export"}>Import .csv</button>
-          <button className={"export"}>Export Current</button>
-        </div>
+      <div>
+        <NodeTable />
+        <EdgeTable />
+      </div>
+      <div>
         <ExitButton />
       </div>
     </div>
   );
 }
 
+// <div className={"sideBySideTables"} style={
+//     {
+//         position: "fixed",
+//         textAlign: "center",
+//
+//     }}>
+//
+// </div>
 export default FileTable;
