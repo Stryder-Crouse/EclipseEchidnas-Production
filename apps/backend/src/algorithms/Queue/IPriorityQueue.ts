95202c809de77d91855c26e34c887f5b1dafc708
Alex <lxrmrz732@gmail.com>
2024-02-05 18:17:53 -0500

    Implemented MapMinPriorityQueue.ts and added test cases.

--- apps/backend/src/algorithms/Queue/IPriorityQueue.ts
+++ apps/backend/src/algorithms/Queue/IPriorityQueue.ts
@@ -1,17 +1,33 @@
 /* Cool interface. Source: https://itnext.io/priority-queue-in-typescript-6ef23116901 */
 export interface IPriorityQueue<T> {
-    /* Put something on the priority queue */
+    /**
+     * Push a thing of type T with a numerical priority onto the queue.
+     * @param thing the thing to push
+     * @param priority the priority of the thing
+     */
     push(thing: T, priority: number): void;
 
-    /* Check the highest priority item without removing it */
+    /**
+     * Check the highest priority item without removing it.
+     * @returns the highest priority item; null if the queue is empty.
+     */
     peek(): T | null;
 
-    /* Check and remove the highest priority item */
+    /**
+     * Check and remove the highest priority item.
+     * @returns the highest priority item; null if the queue is empty.
+     */
     pop(): T | null;
 
-    /* Length of the priority queue */
+    /**
+     * Length of the priority queue.
+     * @returns the length of the queue
+     */
     size(): number;
 
-    /* true iff size() == 0 */
+    /**
+     * Return true if the queue has no elements.
+     * @returns true iff size() == 0;
+     */
     isEmpty(): boolean;
 }
