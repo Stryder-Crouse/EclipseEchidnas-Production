c930be6bfd9fc118aa887330dd72c4f76e1b965c
Alex <lxrmrz732@gmail.com>
2024-02-06 15:04:04 -0500

    Initial boilerplate.

---
+++ apps/backend/src/algorithms/Search/AStar.ts
@@ -0,0 +1,28 @@
+import {Node} from "../Graph/Node.ts";
+import {Graph} from "../Graph/Graph.ts";
+
+/**
+ * A* search algorithm to find a path from source to target on the passed
+ * weighted graph.
+ *
+ * @param source - staring node obj for the search
+ * @param target - target node obj for the search
+ * @param graph - graph which contains both nodes
+ *
+ * @returns  an array of node that stores the path from source to target if it exists or null
+ *  if no such path exists in graph.
+ *
+ */
+export function AStar(source: Node | null, target: Node | null, graph: Graph): Array<Node> | null {
+    /* garbage */
+    if (source == null) {
+        console.error("source in A* was null");
+        return null;
+    }
+    if (target == null) {
+        console.error("target in A* was null");
+        return null;
+    }
+    console.log(graph);
+    return null;
+}
