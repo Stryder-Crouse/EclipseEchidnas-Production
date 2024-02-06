6fbd2c4191cac1adcc4e3556c6fc5776c248c439
Stryder Crouse <strydercrouse@gmail.com>
2024-02-06 18:49:50 -0500

    fixed population code not population with pages afther auth 0 on the map page

--- apps/frontend/src/routes/admin-routes/RequestList.tsx
+++ apps/frontend/src/routes/admin-routes/RequestList.tsx
@@ -3,37 +3,37 @@ import ExitButton from "../../components/buttons/ExitButton.tsx";
 import AdminPageNavBar from "../../components/navigation-bar/AdminPageNavBar.tsx";
 import "../../css/route-css/requestList.css";
 import axios from "axios";
-import {MedReq, ServiceRequest} from "../../../../backend/src/algorithms/Requests/Request.ts";
+import {MedReq} from "../../../../backend/src/algorithms/Requests/Request.ts";
 
 function RequestList() {
-  return (
-    <div>
-      <AdminPageNavBar />
+    return (
+        <div>
+            <AdminPageNavBar />
 
-        <div className={"request-table-container"}>
-          <div className={"table-container"}>
+            <div className={"request-table-container"}>
+                <div className={"table-container"}>
             <span className={"caption-container"}>
               <span className={"table-title"}>Request Log</span>
             </span>
-            <div className={"table-wrapper"}>
-              <table className={"requestTable"} id={"request-table"}>
-                <thead>
-                  <tr>
-                    <th>Request Type</th>
-                    <th>Going To</th>
-                    <th>Medicine type</th>
-                    <th>Dosage</th>
-                    <th>Amount</th>
-                  </tr>
-                </thead>
-                {/* populating here */}
-              </table>
+                    <div className={"table-wrapper"}>
+                        <table className={"requestTable"} id={"request-table"}>
+                            <thead>
+                            <tr>
+                                <th>Request Type</th>
+                                <th>Going To</th>
+                                <th>Medicine type</th>
+                                <th>Dosage</th>
+                                <th>Amount</th>
+                            </tr>
+                            </thead>
+                            {/* populating here */}
+                        </table>
+                    </div>
+                    <ExitButton />
+                </div>
             </div>
-            <ExitButton />
-          </div>
         </div>
-    </div>
-  );
+    );
 }
 
 //may need onload for other ones as well
@@ -42,9 +42,9 @@ populateRequests().then();
 
 //test type REMOVE LATER
 export type request = {
-  startLocation: string;
-  endLocation: string;
-  requestType: string;
+    startLocation: string;
+    endLocation: string;
+    requestType: string;
 };
 
 /**
@@ -52,50 +52,51 @@ export type request = {
  * SHOULD ONLY BE RUN ONCE.
  */
 async function populateRequests() {
-  console.log("RAN");
+    console.log("RAN");
 
-  const requests = await axios.get<ServiceRequest[]>("/api/serviceRequests/medReq");
+    const requests = await axios.get<MedReq[]>("/api/serviceRequests/medReq");
 
-  //fine dropdown div in the html on the page
-  const table = document.getElementById("request-table");
+    //fine dropdown div in the html on the page
+    const table = document.getElementById("request-table");
 
-  console.log(table);
+    console.log(table);
+    console.log(requests.data + "hi");
 
-  //for each node
-  medReqs.data.forEach(function (newRequest: MedReq) {
-    //create tr element to store the record
-    const tableRow = document.createElement("tr");
-    //create td tags for data from record
-    const reqType = document.createElement("td");
-    reqType.textContent = "Medicine request";
-    reqType.setAttribute("class", "node-id");
+    //for each node
+    requests.data.forEach(function (newRequest: MedReq) {
+        //create tr element to store the record
+        const tableRow = document.createElement("tr");
+        //create td tags for data from record
+        const reqType = document.createElement("td");
+        reqType.textContent = "Medicine request";
+        reqType.setAttribute("class", "node-id");
 
-    const reqStartLoc = document.createElement("td");
-    reqStartLoc.textContent = newRequest.reqLocationID;
+        const reqStartLoc = document.createElement("td");
+        reqStartLoc.textContent = newRequest.genReq.toString();
 
-    const reqMedType = document.createElement("td");
-    reqMedType.textContent = newRequest.medType;
+        const reqMedType = document.createElement("td");
+        reqMedType.textContent = newRequest.medType;
 
-    const reqDosage = document.createElement("td");
-    reqDosage.textContent = newRequest.dosage;
+        const reqDosage = document.createElement("td");
+        reqDosage.textContent = newRequest.dosage;
 
-    const reqAmount = document.createElement("td");
-    reqAmount.textContent = newRequest.numDoses.toString();
+        const reqAmount = document.createElement("td");
+        reqAmount.textContent = newRequest.numDoses.toString();
 
-    //append data elements together to one row
-    tableRow.appendChild(reqType);
-    tableRow.appendChild(reqStartLoc);
-    tableRow.appendChild(reqMedType);
-    tableRow.appendChild(reqDosage);
-    tableRow.appendChild(reqAmount);
+        //append data elements together to one row
+        tableRow.appendChild(reqType);
+        tableRow.appendChild(reqStartLoc);
+        tableRow.appendChild(reqMedType);
+        tableRow.appendChild(reqDosage);
+        tableRow.appendChild(reqAmount);
 
-    if (table == null) {
-      return;
-    }
+        if (table == null) {
+            return;
+        }
 
-    //add new row element to table
-    table.appendChild(tableRow);
-  });
+        //add new row element to table
+        table.appendChild(tableRow);
+    });
 }
 
 // async function getRequests() {
@@ -111,3 +112,4 @@ async function populateRequests() {
 // }
 
 export default RequestList;
+
