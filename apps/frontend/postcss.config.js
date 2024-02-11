c4ff872e02d437fb7784007fb2e3b319602428a2
Christopher Lam <127339027+clam365@users.noreply.github.com>
2024-02-11 03:34:16 -0500

    Tailwind Sidebar Feature

--- apps/frontend/postcss.config.js
+++ apps/frontend/postcss.config.js
@@ -1,6 +1,7 @@
 export default {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: { config: './tailwind.config.js' },
     autoprefixer: {},
+
   },
 };
---
+++ apps/frontend/src/routes/TailwindMapPage.tsx
@@ -0,0 +1,26 @@
+import SideNavBarComponent, {SideBarItem} from "../components/SideNavBarComponent.tsx";
+import MapIcon from "../images/SideBar/map.png";
+import ServiceRequestIcon from "../images/SideBar/requestIcon.png";
+import EmployeeIcon from "../images/SideBar/user.png";
+import CSVIcon from "../images/SideBar/table.png";
+import LogIcon from "../images/SideBar/log-in.png";
+function TailwindMapPage() {
+
+    return(
+        <div className="flex">
+            <SideNavBarComponent>
+                <SideBarItem icon={MapIcon} text="Map" link="/TailwindMapPage" />
+                <SideBarItem icon={ServiceRequestIcon} text="Request" link="ServiceRequest"/>
+                <SideBarItem icon={EmployeeIcon} text="Employees" link="/EmployeeTable"/>
+                <SideBarItem icon={CSVIcon} text=".CSV" link="/NodeEdgeTable"/>
+                <hr className="my-3"/>
+                {/*NEED THIS FIXED OR SUM */}
+                <SideBarItem icon={LogIcon} text="Login" link="/ServiceRequest"/>
+
+            </SideNavBarComponent>
+        </div>
+    );
+}
+
+
+export default TailwindMapPage;
