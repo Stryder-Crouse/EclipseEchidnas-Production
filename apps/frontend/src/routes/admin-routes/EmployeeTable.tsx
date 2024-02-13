7c59d61e67bb4ccc5fc6ce4dbe78e05cf731cfb5
Christopher Lam <127339027+clam365@users.noreply.github.com>
2024-02-13 16:06:34 -0500

    Changes by StryderG15

--- apps/frontend/src/routes/admin-routes/EmployeeTable.tsx
+++ apps/frontend/src/routes/admin-routes/EmployeeTable.tsx
@@ -5,19 +5,23 @@ import CSVIcon from "../../images/SideBar/table.png";
 import LogIcon from "../../images/SideBar/log-in.png";
 import MapIcon from "../../images/SideBar/map.png";
 import "../../css/route-css/EmployeeTable.css";
-import React, { useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {Employee} from "../../../../backend/src/algorithms/Employee/Employee.ts";
 
+//TODO IMPLEMENT THESE BUTTONS TO POPULATE WITH EVERY ROW
+//import TrashIcon from "../../images/Table Functions/trash-2.png";
+//import EditIcon from "../../images/Table Functions/pencil.png";
+
 function EmployeeTable() {
+    
+    
+    const [employees, setEmployees] = useState<Employee[]>([]);
+    
+    
+    /* populate the requests */
     useEffect(()=>{
-        let ran;
-        if (!ran) {
-            populateRequests().then();
-
-            ran = true;
-        }
-
+        getEmployees().then((result)=>setEmployees(result));
     },[]);
     //table-id is request-table
     return (
@@ -33,11 +37,36 @@ function EmployeeTable() {
                     <SideBarItem icon={LogIcon} text="Login" link={"/ServiceRequest"}/>
                 </SideNavBarComponent>
             </div>
-
             <div className={"employee-table-container"}>
+                <div className="flex">
                     <span className={"employee-caption-container"}>
                         <span className={"employee-table-title"}>Employee Table</span>
+                        <button onClick={openForm} className="items- drop-shadow-lg transition-all hover:bg-navy w-48 text-white p-2 bg-navStart rounded-full h-min font-semibold ">Add Employee</button>
+
+                        {/* TODO ADD THIS FUNCTION INTO IT */}
+                            <div id={"addEmployeeFOrm"} className="hidden z-20">
+                                <form>
+                                    <label form={"employeeUsername"}>Username</label>
+                                    <input type={"text"} placeholder={"Enter Username"} name={"employeeUsername"} required/>
+                                    
+                                    <label form={"employeeFirst"}>First Name</label>
+                                    <input type={"text"} placeholder={"Enter First Name"} name={"employeeFirst"} required/>
+                                    
+                                    <label form={"employeeLast"}>Last Name</label>
+                                    <input type={"text"} placeholder={"Enter Last Name"} name={"employeeFirst"} required/>
+                                    
+                                    <label form={"designation"}>Designation</label>
+                                    <input type={"text"} placeholder={"Enter Designation"} name={"designation"} required/>
+                                    
+                                    <button type={"submit"}>Add Employee</button>
+                                    <button type={"button"} onClick={closeForm}>Close</button>
+                                </form>
+                            </div>
+
                     </span>
+                </div>
+
+
                 <div className={"employee-table-wrapper"}>
                     <table className={"requestTable"} id={"request-table"}>
                         <thead>
@@ -46,9 +75,26 @@ function EmployeeTable() {
                             <th>First Name</th>
                             <th>Last Name</th>
                             <th>Designation</th>
+                            <th>Actions</th>
                         </tr>
                         </thead>
                         {/* populating here */}
+                        <tbody>
+                        {
+                            employees.map((employee) => {
+                                return (
+                                    <tr key={"Employee_" + employee.userName}>
+                                        <td>{employee.userName}</td>
+                                        <td>{employee.firstName}</td>
+                                        <td>{employee.lastName}</td>
+                                        <td>{employee.designation}</td>
+                                        <td>{employee.isAdmin}</td>
+                                    </tr>
+                                );
+
+                            })
+                        }
+                        </tbody>
                     </table>
                 </div>
             </div>
@@ -60,53 +106,82 @@ function EmployeeTable() {
     );
 }
 
-async function populateRequests() {
-    console.log("RAN");
-
-    const empReqs = await axios.get<Employee[]>("/api/employees/employees");
-
-    //fine dropdown div in the html on the page
-    const table = document.getElementById("request-table");
-
-    console.log(table);
-
-    //for each node
-    empReqs.data.forEach(function (newRequest: Employee) {
-        //create tr element to store the record
-        const tableRow = document.createElement("tr");
-
-        // //create td tags for data from record
-        // const reqType = document.createElement("td");
-        // reqType.textContent = "Medicine request";
-        // reqType.setAttribute("className", "node-id");
-
-        const userName = document.createElement("td");
-        userName.textContent = newRequest.userName;
-        userName.setAttribute("className", "node-id");
-
-        const firstName = document.createElement("td");
-        firstName.textContent = newRequest.firstName;
-
-        const lastName = document.createElement("td");
-        lastName.textContent = newRequest.lastName;
-
-        const designation = document.createElement("td");
-        designation.textContent = newRequest.designation.toString();
-
-        //append data elements together to one row
-        tableRow.appendChild(userName);
-        tableRow.appendChild(firstName);
-        tableRow.appendChild(lastName);
-        tableRow.appendChild(designation);
-        //tableRow.appendChild(reqAmount);
-
-        if (table == null) {
-            return;
-        }
-
-        //add new row element to table
-        table.appendChild(tableRow);
-    });
+/**
+ * open the div add Employee Form
+ */
+function openForm() {
+    const openSesame = document.getElementById("addEmployeeForm");
+    if (openSesame != null) {
+        openSesame.style.display= "block";
+    }
 }
+
+/**
+ * close the div add Employee Form
+ */
+function closeForm() {
+    const close = document.getElementById("addEmployeeForm");
+    if (close != null) {
+        close.style.display= "none";
+    }
+}
+
+async function getEmployees() {
+    const employees = await
+        axios.get<Employee[]>("/api/employees/employees");
+    return employees.data;
+}
+
+//TODO this function needs to replaced by actually doing the right populating, follow homework 3
+// async function populateRequests() {
+//     /* useless comment */
+//     console.log("RAN");
+//
+//     /* get the employees from backend */
+//     const empReqs = await axios.get<Employee[]>("/api/employees/employees");
+//
+//     //fine dropdown div in the html on the page
+//     const table = document.getElementById("request-table");
+//
+//     console.log(table);
+//
+//     //for each node
+//     empReqs.data.forEach(function (newRequest: Employee) {
+//         //create tr element to store the record
+//         const tableRow = document.createElement("tr");
+//
+//         // //create td tags for data from record
+//         // const reqType = document.createElement("td");
+//         // reqType.textContent = "Medicine request";
+//         // reqType.setAttribute("className", "node-id");
+//
+//         const userName = document.createElement("td");
+//         userName.textContent = newRequest.userName;
+//         userName.setAttribute("className", "node-id");
+//
+//         const firstName = document.createElement("td");
+//         firstName.textContent = newRequest.firstName;
+//
+//         const lastName = document.createElement("td");
+//         lastName.textContent = newRequest.lastName;
+//
+//         const designation = document.createElement("td");
+//         designation.textContent = newRequest.designation.toString();
+//
+//         //append data elements together to one row
+//         tableRow.appendChild(userName);
+//         tableRow.appendChild(firstName);
+//         tableRow.appendChild(lastName);
+//         tableRow.appendChild(designation);
+//         //tableRow.appendChild(reqAmount);
+//
+//         if (table == null) {
+//             return;
+//         }
+//
+//         //add new row element to table
+//         table.appendChild(tableRow);
+//     });
+// }
 
 export default EmployeeTable;
