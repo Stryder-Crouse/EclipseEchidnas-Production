e9d375f587dc7f7f254e8f724b37ba869e1a1326
Stryder Crouse <strydercrouse@gmail.com>
2024-02-13 19:27:33 -0500

    set up

---
+++ apps/frontend/src/components/service-requests/service-request/ServiceRequest_Table.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {statusFilter} from "../serviceRequestInterface.ts";
+
+
+
+
+export default function ServiceRequest_Table({statusFilter:statusFilter}:statusFilter) {
+    console.log(statusFilter);
+
+    return (
+        <div >
+            {/* make your table in here  */}
+            Religious request table
+        </div>
+    );
+
+
+}
