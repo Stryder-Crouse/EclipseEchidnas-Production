69b80ff62c9adad5e63a8e497f6abfdebede5287
Mike Conroy <98478094+RentalPenny@users.noreply.github.com>
2024-02-20 11:08:58 -0500

    Refactored to the login to be back to universal login, and set up checking if user is already in the database, and if not lets them register their name and username before storing them in the database

--- apps/frontend/src/routes/admin-routes/EmployeeTable.tsx
+++ apps/frontend/src/routes/admin-routes/EmployeeTable.tsx
@@ -209,6 +209,7 @@ function EmployeeTable() {
         }
 
         const newEmployee: Employee = {
+            accessToken: '',
             designation: newDesignation ,
             firstName: newFristName,
             isAdmin: newIsAdmin,
