141856ee7fd80883b569f38a8626d0235395f94c
Sameer <sameer.augustine@gmail.com>
2024-02-20 16:45:54 -0500

    about page stuff

--- apps/frontend/src/components/AboutPage.tsx
+++ apps/frontend/src/components/AboutPage.tsx
@@ -13,6 +13,10 @@ import SzymPic from "../images/Team/Szymon.png";
 import GracePic from "../images/Team/Grace.png";
 import AJPic from "../images/Team/AJ.png";
 import AlanaPic from "../images/Team/Alana.png";
+import ShiivekPic from "../images/Team/Shiivek.jpg";
+import AlexPic from "../images/Team/Alex.png";
+import StryderPic from "../images/Team/Stryder.png";
+import WongPic from "../images/Team/Wong.jpg";
 
 function AboutPage(){
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -24,10 +28,10 @@ function AboutPage(){
             backgroundRepeat: "no-repeat",
         };
     return(
-        <>
+        <div className={"flex h-lvh flex-row"}>
 
             <div className="flex">
-                <div className="flex absolute w-screen h-100">
+                <div className="z-10">
                     <SideNavBarComponent>
                         <SideBarItem icon={MapIcon} text="Map" link={window.location.pathname}/>
                         <SideBarItem icon={ServiceRequestIcon} text="Services" link="ServiceRequest"/>
@@ -39,188 +43,85 @@ function AboutPage(){
                     </SideNavBarComponent>
                 </div>
             </div>
-            {/*<h1 className="font-bold text-center ...">ABOUT</h1>*/}
-            {/*<p className="font-bold text-center ...">DEPARTMENT</p>*/}
-            {/*<p className="text-center ...">WPI Computer Science Department</p>*/}
-            {/*<p className="font-bold text-center ...">COURSE</p>*/}
-            {/*<p className="text-center ...">CS3733-C24 Software Engineering</p>*/}
-            {/*<p className="font-bold text-center ...">INSTRUCTOR</p>*/}
-            {/*<p className="text-center ...">Prof. Wilson Wong</p>*/}
-            {/*<p className="font-bold text-center ...">TEAM COACH</p>*/}
-            {/*<p className="text-center ...">Joseph Cardarelli</p>*/}
-            {/*<p className="font-bold text-center ...">THE TEAM</p>*/}
-            <p className="font-bold text-center ...">THE TEAM</p>
-            <div className="grid grid-cols-6 gap-1">
-                <div></div>
-                <div>
+            <div className="flex flex-col m-auto">
+                <p className="flex font-bold text-center">THE TEAM</p>
+                <div className="flex flex-row">
+                    <div className="flex grid grid-cols-6 gap-4">
+                        <ImageCard img={StryderPic} name={"Stryder Crouse"} role={"Team Lead"}>
+                        </ImageCard>
+                        <ImageCard img={GracePic} name={"Grace Philips"} role={"Assistant Lead"}>
+                        </ImageCard>
+                        <ImageCard img={ChrisPic} name={"Chris Lam"} role={"Assistant Lead"}>
+                        </ImageCard>
+                        <ImageCard img={ShiivekPic} name={"Shiivek Agarwal"} role={"Project Manager"}>
+                        </ImageCard>
+                        <ImageCard img={SameerPic} name={"Sameer Augustine"} role={"Scrum Master"}>
+                        </ImageCard>
+                        <ImageCard img={AlanaPic} name={"Alana Reid"} role={"Product Owner"}>
+                        </ImageCard>
+                        <ImageCard img={MikePic} name={"Mike Conroy"} role={"Documentation Analyst"}>
+                        </ImageCard>
+                        <ImageCard img={AJPic} name={"Antonio Aguiar"} role={"Frontend Engineer"}>
+                        </ImageCard>
+                        <ImageCard img={SzymPic} name={"Szymon Mamro"} role={"Backend Engineer"}>
+                        </ImageCard>
+                        <ImageCard img={RyanPic} name={"Ryan Hunter"} role={"Backend Engineer"}>
+                        </ImageCard>
+                        <ImageCard img={AlexPic} name={"Alex Ramirez"} role={"Algorithms Engineer"}>
+                        </ImageCard>
+                        <ImageCard img={WongPic} name={"Wilson Wong"} role={"Professor"}>
+                        </ImageCard>
+                    </div>
                     <div
-                        className="scale-75 max-w-sm rounded overflow-hidden shadow-lg text-black bg-ivoryWhite text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all">
-                        <img className="h-60 w-full " src={SameerPic} alt="Stryder"/>
-                        <div className="px-6 py-4">
-                            <div className="font-bold text-xl mb-2 text-center">Stryder Crouse</div>
-                            <p className="text-gray-700 text-base text-center">
-                                Team Lead
-                            </p>
-                        </div>
-
+                        className="flex justify-center flex-col m-4 h-332 w-[200px] p-4 border-4">
+                        <h1 className=" flex font-bold text-center ...">ABOUT</h1>
+                        <p className="flex font-bold text-center ...">DEPARTMENT</p>
+                        <p className="flex text-center ...">WPI Computer Science Department</p>
+                        <p className="flex font-bold text-center ...">COURSE</p>
+                        <p className="flex text-center ...">CS3733-C24 Software Engineering</p>
+                        <p className="flex font-bold text-center ...">INSTRUCTOR</p>
+                        <p className="flex text-center ...">Prof. Wilson Wong</p>
+                        <p className="flex font-bold text-center ...">TEAM COACH</p>
+                        <p className="flex text-center ...">Joseph Cardarelli</p>
+                        <p className="flex font-bold text-center ...">SPECIAL THANKS</p>
+                        <p className="flex text-center ...">A special thank you to Brigham and Women’s Hospital
+                            for their continued partnership and to Andrew Shin for taking the
+                            time to come and review the work done by the teams.</p>
                     </div>
                 </div>
-                <div
-                    className="scale-75 max-w-sm rounded overflow-hidden shadow-lg text-black bg-ivoryWhite text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all">
-                    <img className="h-60 w-full" src={GracePic} alt="Sunset in the mountains"/>
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2 text-center">Grace Philips</div>
-                        <p className="text-gray-700 text-base text-center">
-                            Assistant Lead
-                        </p>
+                <footer className="align-self-end bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+                    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                    <span className="text-center text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                        © The Brigham & Women’s Hospital maps and data used in this
+                        application are copyrighted and provided for the sole use of educational purposes.
+                    </span>
                     </div>
-
-                </div>
-
-                <div
-                    className="scale-75 max-w-sm rounded overflow-hidden shadow-lg text-black bg-ivoryWhite text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all">
-                    <img className="h-60 w-full" src={ChrisPic} alt="Sunset in the mountains"/>
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2 text-center">Chris Lam</div>
-                        <p className="text-gray-700 text-base text-center">
-                            Assistant Lead
-                        </p>
-                    </div>
-
-                </div>
-
-
-                <div
-                    className="scale-75 max-w-sm rounded overflow-hidden shadow-lg text-black bg-ivoryWhite text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all">
-                    <img className="h-60 w-full" src={SameerPic} alt="Sunset in the mountains"/>
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2 text-center">Shiivek Agarwal</div>
-                        <p className="text-gray-700 text-base text-center">
-                            Project Manager
-                        </p>
-                    </div>
-
-                </div>
-                <div
-                    className="border-2 col-span-1 row-span-2 box-border h-332 w-350 p-4 border-4 ...">
-                    <h1 className="font-bold text-center ...">ABOUT</h1>
-                    <p className="font-bold text-center ...">DEPARTMENT</p>
-                    <p className="text-center ...">WPI Computer Science Department</p>
-                    <p className="font-bold text-center ...">COURSE</p>
-                    <p className="text-center ...">CS3733-C24 Software Engineering</p>
-                    <p className="font-bold text-center ...">INSTRUCTOR</p>
-                    <p className="text-center ...">Prof. Wilson Wong</p>
-                    <p className="font-bold text-center ...">TEAM COACH</p>
-                    <p className="text-center ...">Joseph Cardarelli</p>
-                    <p className="font-bold text-center ...">SPECIAL THANKS</p>
-                    <p className="text-center ...">A special thank you to Brigham and Women’s Hospital
-                        for their continued partnership and to Andrew Shin for taking the
-                        time to come and review the work done by the teams.</p>
-                </div>
-                <div></div>
-                <div
-                    className="scale-75 max-w-sm rounded overflow-hidden shadow-lg text-black bg-ivoryWhite text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all">
-                    <img className="h-60 w-full" src={SameerPic} alt="Sunset in the mountains"/>
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2 text-center">Sameer Augustine</div>
-                        <p className="text-gray-700 text-base text-center">
-                            Scrum Master
-                        </p>
-                    </div>
-
-                </div>
-                <div
-                    className="scale-75 max-w-sm rounded overflow-hidden shadow-lg text-black bg-ivoryWhite text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all">
-                    <img className="h-60 w-full" src={AlanaPic} alt="Sunset in the mountains"/>
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2 text-center">Alana Reid</div>
-                        <p className="text-gray-700 text-base text-center">
-                            Product Owner
-                        </p>
-                    </div>
-
-                </div>
-
-
-                <div
-                    className="scale-75 max-w-sm rounded overflow-hidden shadow-lg text-black bg-ivoryWhite text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all">
-                    <img className="h-60 w-full" src={MikePic} alt="Sunset in the mountains"/>
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2 text-center">Mike Conroy</div>
-                        <p className="text-gray-700 text-base text-center">
-                            Documentation Analyst
-                        </p>
-                    </div>
-
-                </div>
-                <div
-                    className="scale-75 max-w-sm rounded overflow-hidden shadow-lg text-black bg-ivoryWhite text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all">
-                    <img className="h-60 w-full" src={AJPic} alt="Sunset in the mountains"/>
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2 text-center">Antonio Aguiar</div>
-                        <p className="text-gray-700 text-base text-center">
-                            Frontend Engineer
-                        </p>
-                    </div>
-
-                </div>
-                <div></div>
-                <div
-                    className="scale-75 max-w-sm rounded overflow-hidden shadow-lg text-black bg-ivoryWhite text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all">
-                    <img className="h-60 w-full" src={SzymPic} alt="Sunset in the mountains"/>
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-3 text-center">Szymon Mamro</div>
-                        <p className="text-gray-700 text-base text-center">
-                            Backend Engineer
-                        </p>
-                    </div>
-
-                </div>
-                <div
-                    className="scale-75 max-w-sm rounded overflow-hidden shadow-lg text-black bg-ivoryWhite text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all">
-                    <img className="h-60 w-full" src={RyanPic} alt="Sunset in the mountains"/>
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2 text-center">Ryan Hunter</div>
-                        <p className="text-gray-700 text-base text-center">
-                            Backend Engineer
-                        </p>
-                    </div>
-
-                </div>
-
-                <div
-                    className="scale-75 max-w-sm rounded overflow-hidden shadow-lg text-black bg-ivoryWhite text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all">
-                    <img className="h-60 w-full" src={SameerPic} alt="Sunset in the mountains"/>
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2 text-center">Alex Ramirez</div>
-                        <p className="text-gray-700 text-base text-center">
-                            Algorithms Engineer
-                        </p>
-                    </div>
-
-                </div>
-
-
+                </footer>
             </div>
-            <div className="grid grid-cols-7 gap-7">
-                <div></div>
-                <div></div>
+        </div>
+    );
+}
 
+export type imageProps = {
+    img: string, name: string, role: string
+}
 
+export function ImageCard({img, name, role}: imageProps) {
+
+    return (
+        <div
+            className="w-[10vw] rounded overflow-hidden shadow-lg text-black bg-ivoryWhite text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all">
+            <img className="h-fit w-full " src={img} alt="Stryder"/>
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2 text-center">{name}</div>
+                <p className="text-gray-700 text-base text-center">
+                    {role}
+                </p>
             </div>
-
-            {/*<p className="font-bold text-center ...">COPYRIGHT</p>*/}
-            <p className="text-center ..."></p>
-            <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-                <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-center text-sm text-gray-500 sm:text-center dark:text-gray-400">© The Brigham & Women’s Hospital maps and data used in this
-                application are copyrighted and provided for the sole use of educational purposes.</span>
-                </div>
-            </footer>
-        </>
-
+        </div>
 
     );
 }
+
 
 export default AboutPage;
