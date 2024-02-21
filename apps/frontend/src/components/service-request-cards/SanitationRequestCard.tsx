d051e0a6e000cf0492f315c9e73a752b0118c59c
Christopher Lam <127339027+clam365@users.noreply.github.com>
2024-02-21 10:18:50 -0500

    hover bug removed

--- apps/frontend/src/components/service-request-cards/SanitationRequestCard.tsx
+++ apps/frontend/src/components/service-request-cards/SanitationRequestCard.tsx
@@ -18,7 +18,7 @@ export default function SanitationRequestCard() {
         setIsPopupOpen(false);
     }
     return (
-        <div className={"flex mr-12 hover:scale-105 transition-all"}>
+        <div className={"flex mr-12 "}>
             <div className="text-black rounded-lg bg-ivoryWhite w-56 h-72 text-center drop-shadow-xl hover:border-b-teal border-b-8 border-ivoryWhite cursor-pointer transition-all hover:border-t-teal border-t-8" id={"medicineForm"} onClick={openSanitationForm}>
                 <img src={SanitationImage} alt={"sanitationImage"} className="scale-90"/>
                 <h1 className="font-semibold mt-2">SANITATION</h1>
