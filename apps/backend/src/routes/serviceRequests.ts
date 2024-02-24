ab8e81df23bc23a7ef2047af195ed8c5e96abcf7
Szymon <szymonmamro@gmail.com>
2024-02-24 15:32:25 -0500

    Added data holder for BTM building

--- apps/backend/src/routes/serviceRequests.ts
+++ apps/backend/src/routes/serviceRequests.ts
@@ -160,9 +160,25 @@ router.get("/serviceReq/building-statistics", async function (req: Request, res:
             inProgress: 0,
             completed: 0
         };
+        const resultBTM = {
+            total: 0,
+            medReq: 0,
+            religReq: 0,
+            flowReq: 0,
+            sanReq: 0,
+            tranReq: 0,
+            lowPrio: 0,
+            medPrio: 0,
+            highPrio: 0,
+            emergPrio: 0,
+            unassigned: 0,
+            assigned: 0,
+            inProgress: 0,
+            completed: 0
+        };
 
         const result = {
-            resultShapiro, resultTower, result45Francis, result15Francis
+            resultShapiro, resultTower, result45Francis, result15Francis, resultBTM
         };
         console.log(result);
 
