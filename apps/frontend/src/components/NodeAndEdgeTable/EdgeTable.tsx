439c17767faa853d22676f315eaf934f4a3ec112
Stryder Crouse <strydercrouse@gmail.com>
2024-02-23 22:28:48 -0500

    changed more for each loops to for of

--- apps/frontend/src/components/NodeAndEdgeTable/EdgeTable.tsx
+++ apps/frontend/src/components/NodeAndEdgeTable/EdgeTable.tsx
@@ -49,31 +49,59 @@ async function populateEdges() {
   console.log(table);
 
   //for each node
-  edgeData.data.forEach(function (newEdge: EdgeDataBase) {
-    //create tr element to store the record
-    const tableRow = document.createElement("tr");
-    //create td tags for data from record
-    const edgeID = document.createElement("td");
-    edgeID.textContent = newEdge.edgeID;
-    edgeID.setAttribute("class", "node-id");
 
-    const edgeStart = document.createElement("td");
-    edgeStart.textContent = newEdge.startNodeID;
+    for (const newEdge of edgeData.data){
+        //create tr element to store the record
+        const tableRow = document.createElement("tr");
+        //create td tags for data from record
+        const edgeID = document.createElement("td");
+        edgeID.textContent = newEdge.edgeID;
+        edgeID.setAttribute("class", "node-id");
 
-    const edgeEnd = document.createElement("td");
-    edgeEnd.textContent = newEdge.endNodeID;
+        const edgeStart = document.createElement("td");
+        edgeStart.textContent = newEdge.startNodeID;
 
-    //append data elements together to one row
-    tableRow.appendChild(edgeID);
-    tableRow.appendChild(edgeStart);
-    tableRow.appendChild(edgeEnd);
-    if (table == null) {
-      return;
+        const edgeEnd = document.createElement("td");
+        edgeEnd.textContent = newEdge.endNodeID;
+
+        //append data elements together to one row
+        tableRow.appendChild(edgeID);
+        tableRow.appendChild(edgeStart);
+        tableRow.appendChild(edgeEnd);
+        if (table == null) {
+            return;
+        }
+
+        //add new row element to table
+        table.appendChild(tableRow);
     }
 
-    //add new row element to table
-    table.appendChild(tableRow);
-  });
+    //todo remove
+  //   edgeData.data.forEach(function (newEdge: EdgeDataBase) {
+  //   //create tr element to store the record
+  //   const tableRow = document.createElement("tr");
+  //   //create td tags for data from record
+  //   const edgeID = document.createElement("td");
+  //   edgeID.textContent = newEdge.edgeID;
+  //   edgeID.setAttribute("class", "node-id");
+  //
+  //   const edgeStart = document.createElement("td");
+  //   edgeStart.textContent = newEdge.startNodeID;
+  //
+  //   const edgeEnd = document.createElement("td");
+  //   edgeEnd.textContent = newEdge.endNodeID;
+  //
+  //   //append data elements together to one row
+  //   tableRow.appendChild(edgeID);
+  //   tableRow.appendChild(edgeStart);
+  //   tableRow.appendChild(edgeEnd);
+  //   if (table == null) {
+  //     return;
+  //   }
+  //
+  //   //add new row element to table
+  //   table.appendChild(tableRow);
+  // });
 }
 
 export default EdgeTable;
