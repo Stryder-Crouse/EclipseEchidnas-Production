7503bfbf2f9616d7ff4b94f749e9aaa6ee93cbb5
SushiSuun <145725760+SushiSuun@users.noreply.github.com>
2024-02-25 16:34:40 -0500

    Deleted old code which was cluttering up stuff

--- apps/backend/src/routes/loadFromCSVFile.ts
+++ apps/backend/src/routes/loadFromCSVFile.ts
@@ -130,38 +130,4 @@ router.get("/", async function (req: Request, res: Response) {
 });
 
 
-// // Whenever a get request is made to /CSVnode, send back the data from the node file.
-// router.get("/CSVnode", async function (req: Request, res: Response) {
-//   let allNodesString = null;
-//   try {
-//     //calculate file location
-//     const csvLocation = path.resolve(__dirname, "../../resources/L1Nodes.csv");
-//     //read file into string
-//     allNodesString = fs.readFileSync(csvLocation, "utf-8");
-//   } catch (error) {
-//     console.error("CSV node file not found");
-//     res.status(204); // and send 204
-//     return;
-//   }
-//   //send file data if no errors
-//   res.status(200).send(allNodesString);
-// });
-//
-// // Whenever a get request is made to /CSVedge, send back the data from the edge file.
-// router.get("/CSVedge", async function (req: Request, res: Response) {
-//   let allEdgeString = null;
-//   try {
-//     //calculate file location
-//     const csvLocation = path.resolve(__dirname, "../../resources/L1Edges.csv");
-//     //read file into string
-//     allEdgeString = fs.readFileSync(csvLocation, "utf-8");
-//   } catch (error) {
-//     console.error("CSV edge file not found");
-//     res.status(204); // and send 204
-//     return;
-//   }
-//
-//   res.status(200).send(allEdgeString);
-// });
-
 export default router;
