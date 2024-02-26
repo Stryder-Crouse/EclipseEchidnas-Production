e91ca6fcb32f42c2269c7efddedf9d4ddc06e6f0
Alex <lxrmrz732@gmail.com>
2024-02-26 18:01:18 -0500

    bruh?

--- packages/common/src/algorithms/Search/Dijkstra.ts
+++ packages/common/src/algorithms/Search/Dijkstra.ts
@@ -3,7 +3,7 @@ import {Graph} from "../Graph/Graph.ts";
 import {BestFirstSearch} from "./BestFirstSearch.ts";
 
 /**
- * A* search algorithm to find a path from source to target on the passed
+ * Dijkstra search algorithm to find a path from source to target on the passed
  * weighted graph.
  *
  * @param source - staring node obj for the search
@@ -16,10 +16,7 @@ import {BestFirstSearch} from "./BestFirstSearch.ts";
  *  @see {https://en.wikipedia.org/wiki/A*_search_algorithm}
  *
  */
-export function AStar(source: Node | null, target: Node | null, graph: Graph): Array<Node> | null {
-    /* Simple wrapper */
-    if (target != null) {
-        graph.generateNodeHeuristic(target);
-    }
+export function Dijkstra(source: Node | null, target: Node | null, graph: Graph): Array<Node> | null {
+    /* Simple wrapper; heuristic is initialised to zero so this is equivalent to Dijkstra */
     return BestFirstSearch(source, target, graph);
 }
