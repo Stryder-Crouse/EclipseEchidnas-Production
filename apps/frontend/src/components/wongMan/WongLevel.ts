2eca6dd262ed40861458f5b04aa1e7a7477d9213
Alex <lxrmrz732@gmail.com>
2024-02-27 18:07:14 -0500

    Real

--- apps/frontend/src/components/wongMan/WongLevel.ts
+++ apps/frontend/src/components/wongMan/WongLevel.ts
@@ -2,7 +2,7 @@ import {Coordinate} from "common/src/algorithms/Graph/Coordinate.ts";
 import {Edible} from "./Edible.ts";
 
 /**
- * Struct to hold the data
+ * Struct to hold the data for each level
  */
 export type WongLevel = {
     edibles: Array<Edible>;
